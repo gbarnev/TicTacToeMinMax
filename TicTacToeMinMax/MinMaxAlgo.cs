2bcb9357aa5f6f7f50d0f1a3793e43f00113e4f0
Georgi Barnev <gebarnev@progress.com>
2020-11-30 00:05:14 +0200

    Initial commit

---
+++ TicTacToeMinMax/MinMaxAlgo.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToeMinMax
+{
+    public class MinMaxAlgo
+    {
+        public const char EmptySymbol = '_';
+        public const char RobotTurnSymbol = 'O';
+        public const char PersonTurnSymbol = 'X';
+
+        public void NextMove(char[,] board)
+        {
+            int bestRow = -1;
+            int bestCol = -1;
+            var bestScore = int.MinValue;
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    if (board[row, col] != EmptySymbol)
+                    {
+                        continue;
+                    }
+                    board[row, col] = RobotTurnSymbol;
+                    var curScore = this.MinScore(board, int.MinValue, int.MaxValue, 1);
+                    board[row, col] = EmptySymbol;
+                    if (bestScore < curScore)
+                    {
+                        bestRow = row;
+                        bestCol = col;
+                        bestScore = curScore;
+                    }
+                }
+            }
+            board[bestRow, bestCol] = RobotTurnSymbol;
+        }
+
+        private int MaxScore(char[,] board, int alpha, int beta, int depth)
+        {
+            (var hasEnded, var score) = this.HasGameEnded(board);
+            if (hasEnded)
+            {
+                return this.ModifyScoreWithDepth(score.Value, depth);
+            }
+
+            var curMax = int.MinValue;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (board[i, j] != EmptySymbol)
+                    {
+                        continue;
+                    }
+
+                    board[i, j] = RobotTurnSymbol;
+                    var min = this.MinScore(board, alpha, beta, depth + 1);
+                    board[i, j] = EmptySymbol;
+                    curMax = Math.Max(min, curMax);
+                    alpha = Math.Max(min, alpha);
+                    if (alpha >= beta)
+                    {
+                        return curMax;
+                    }
+                }
+            }
+            return curMax;
+        }
+
+        private int MinScore(char[,] board, int alpha, int beta, int depth)
+        {
+            (var hasEnded, var score) = this.HasGameEnded(board);
+            if (hasEnded)
+            {
+                return this.ModifyScoreWithDepth(score.Value, depth);
+            }
+
+            var curMin = int.MaxValue;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (board[i, j] != EmptySymbol)
+                    {
+                        continue;
+                    }
+
+                    board[i, j] = PersonTurnSymbol;
+                    var max = this.MaxScore(board, alpha, beta, depth + 1);
+                    board[i, j] = EmptySymbol;
+                    curMin = Math.Min(max, curMin);
+                    beta = Math.Min(max, beta);
+                    if (alpha >= beta)
+                    {
+                        return curMin;
+                    }
+                }
+            }
+            return curMin;
+        }
+
+        public (bool HasEnded, int? Score) HasGameEnded(char[,] board)
+        {
+            var score = CalculateScore(board);
+            if (score != 0)
+            {
+                return (true, score);
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (board[i, j] == EmptySymbol)
+                    {
+                        return (false, null);
+                    }
+                }
+            }
+            return (true, score);
+        }
+
+        private int CalculateScore(char[,] board)
+        {
+            //right diagonal
+            if (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2] && board[0, 0] != EmptySymbol)
+            {
+                return board[0, 0] == RobotTurnSymbol ? 10 : -10;
+            }
+
+            //left diagonal
+            if (board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0] && board[2, 0] != EmptySymbol)
+            {
+                return board[0, 2] == RobotTurnSymbol ? 10 : -10;
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[i, 0] == board[i, 1] && board[i, 1] == board[i, 2] && board[i, 0] != EmptySymbol)
+                {
+                    return board[i, 0] == RobotTurnSymbol ? 10 : -10;
+                }
+                if (board[0, i] == board[1, i] && board[1, i] == board[2, i] && board[0, i] != EmptySymbol)
+                {
+                    return board[0, i] == RobotTurnSymbol ? 10 : -10;
+                }
+            }
+            return 0;
+        }
+
+        private int ModifyScoreWithDepth(int score, int depth)
+        {
+            if (score == 0)
+            {
+                return score;
+            }
+            else if (score > 0)
+            {
+                return score - depth;
+            }
+            else
+            {
+                return score + depth;
+            }
+        }
+    }
+}
---
+++ TicTacToeMinMax/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+
+namespace TicTacToeMinMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, enter who is first(1 for robot, 0 for you):");
+            var isRobotTurn = int.Parse(Console.ReadLine()) == 1;
+            var board = new char[3, 3];
+            InitBoard(board);
+            var algo = new MinMaxAlgo();
+
+            while (true)
+            {
+                var result = algo.HasGameEnded(board);
+                if (result.HasEnded)
+                {
+                    if (result.Score.Value > 0)
+                    {
+                        Console.WriteLine("Robot won the game!");
+                    }
+                    else if (result.Score.Value == 0)
+                    {
+                        Console.WriteLine("Tie!");
+                    }
+                    else
+                    {
+                        Console.Write("Congratulations! You won!");
+                    }
+                    break;
+                }
+
+                if (isRobotTurn)
+                {
+                    Console.WriteLine("Robot played: ");
+                    algo.NextMove(board);
+                    isRobotTurn = false;
+                }
+                else
+                {
+                    Console.WriteLine("Please, place your turn:");
+                    PersonMove(board);
+                    isRobotTurn = true;
+                }
+                PrintBoard(board);
+            }
+        }
+
+        private static void PersonMove(char[,] board)
+        {
+            var move = Console.ReadLine();
+            var rowCol = move.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            var parsedRowCol = rowCol.Select(x => int.Parse(x)).ToArray();
+
+            while (
+                parsedRowCol[0] > 2 || parsedRowCol[0] < 0 || 
+                parsedRowCol[1] > 2 || parsedRowCol[1] < 0 ||
+                board[parsedRowCol[0], parsedRowCol[1]] != MinMaxAlgo.EmptySymbol)
+            {
+                Console.WriteLine("Wrong input. Please enter new values:");
+                move = Console.ReadLine();
+                rowCol = move.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                parsedRowCol = rowCol.Select(x => int.Parse(x)).ToArray();
+            }
+
+            board[parsedRowCol[0], parsedRowCol[1]] = MinMaxAlgo.PersonTurnSymbol;
+        }
+
+        private static void InitBoard(char[,] board)
+        {
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    board[i, j] = MinMaxAlgo.EmptySymbol;
+        }
+
+        private static void PrintBoard(char[,] board)
+        {
+            var sb = new StringBuilder();
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    sb.Append(board[i, j] + " ");
+                }
+                sb.AppendLine();
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
